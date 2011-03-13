1e0ed4f90f68fc4446e24fc40c8ead4b11f1997d
bneate <blair@leftclick.com>
2011-03-14 11:08:34 +1300

    added customer

---
+++ app/models/breeze/account/customer.rb
@@ -0,0 +1,14 @@
+module Breeze
+  module Account
+    class Customer
+      include Mongoid::Document
+
+      # Include default devise modules. Others available are:
+      # :token_authenticatable, :confirmable, :lockable and :timeoutable
+      devise :database_authenticatable, :registerable,
+             :recoverable, :rememberable, :trackable, :validatable
+
+    end
+  end
+end
+
