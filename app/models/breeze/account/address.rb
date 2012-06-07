b7ba8a4d4828f76b7d79fef360fef1f1616c32a3
bneate <blair@leftclick.com>
2012-06-07 15:39:51 +1200

    making sure everything is checked in

--- app/models/breeze/account/address.rb
+++ app/models/breeze/account/address.rb
@@ -3,9 +3,18 @@ module Breeze
     class Address
       include Mongoid::Document
      
+      field :first_name
+      field :last_name
       field :address_line_one
       field :address_line_two
+      field :address_line_three
+      field :region
+      field :city
       field :country
+      field :postcode
+
+      embedded_in :customer, :class_name => "Breeze::Account::Customer", :inverse_of => :billing_address
+      embedded_in :customer, :class_name => "Breeze::Account::Customer", :inverse_of => :shipping_address
     end
   end
 end
--- app/models/breeze/account/customer.rb
+++ app/models/breeze/account/customer.rb
@@ -3,14 +3,29 @@ module Breeze
     class Customer
       include Mongoid::Document
 
-
+      field :title
       field :first_name
       field :last_name
+      field :phone
+
+      embeds_one :billing_address, :class_name => "Breeze::Account::Address"
+      embeds_one :shipping_address, :class_name => "Breeze::Account::Address"
+
+      field :marketing, :type => Boolean
+
+      accepts_nested_attributes_for :billing_address
+      accepts_nested_attributes_for :shipping_address
 
       # Include default devise modules. Others available are:
       # :token_authenticatable, :confirmable, :lockable and :timeoutable
       devise :database_authenticatable, :registerable,
              :recoverable, :rememberable, :trackable, :validatable
+
+      scope :marketing, where(:marketing => true)
+
+      def name
+        [ first_name, last_name ].reject { |c| c.blank? }.join(" ")
+      end
 
     end
   end
