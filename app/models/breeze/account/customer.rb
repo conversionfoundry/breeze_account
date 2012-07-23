174d95484e1581c7409387122e065b128068b846
Isaac Freeman <isaac@leftclick.com>
2012-07-23 15:33:02 +1200

    Moved Devise fields to a mixing Added Component Info hook

--- app/models/breeze/account/customer.rb
+++ app/models/breeze/account/customer.rb
@@ -20,48 +20,49 @@ module Breeze
       validates_presence_of :password, :password_confirmation, :if => :new_record?
       validates_confirmation_of :password
 
+      include Breeze::Admin::Mixins::Login
+
       # Include default devise modules. Others available are:
       # :token_authenticatable, :confirmable, :lockable and :timeoutable
-      devise :database_authenticatable, :registerable,
-             :recoverable, :rememberable, :trackable #, :validatable
+      # devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable #, :validatable
              
-       ## Database authenticatable
-       field :email,              :type => String, :null => false
-       field :encrypted_password, :type => String, :null => false
+       # ## Database authenticatable
+       # field :email,              :type => String, :null => false
+       # field :encrypted_password, :type => String, :null => false
 
-       ## Recoverable
-       field :reset_password_token,   :type => String
-       field :reset_password_sent_at, :type => Time
+       # ## Recoverable
+       # field :reset_password_token,   :type => String
+       # field :reset_password_sent_at, :type => Time
 
-       ## Rememberable
-       field :remember_created_at, :type => Time
+       # ## Rememberable
+       # field :remember_created_at, :type => Time
 
-       ## Trackable
-       field :sign_in_count,      :type => Integer
-       field :current_sign_in_at, :type => Time
-       field :last_sign_in_at,    :type => Time
-       field :current_sign_in_ip, :type => String
-       field :last_sign_in_ip,    :type => String
+       # ## Trackable
+       # field :sign_in_count,      :type => Integer
+       # field :current_sign_in_at, :type => Time
+       # field :last_sign_in_at,    :type => Time
+       # field :current_sign_in_ip, :type => String
+       # field :last_sign_in_ip,    :type => String
 
-       ## Encryptable
-       # field :password_salt, :type => String
+       # ## Encryptable
+       # # field :password_salt, :type => String
 
-       ## Confirmable
-       # field :confirmation_token,   :type => String
-       # field :confirmed_at,         :type => Time
-       # field :confirmation_sent_at, :type => Time
-       # field :unconfirmed_email,    :type => String # Only if using reconfirmable
+       # ## Confirmable
+       # # field :confirmation_token,   :type => String
+       # # field :confirmed_at,         :type => Time
+       # # field :confirmation_sent_at, :type => Time
+       # # field :unconfirmed_email,    :type => String # Only if using reconfirmable
 
-       ## Lockable
-       # field :failed_attempts, :type => Integer # Only if lock strategy is :failed_attempts
-       # field :unlock_token,    :type => String # Only if unlock strategy is :email or :both
-       # field :locked_at,       :type => Time
+       # ## Lockable
+       # # field :failed_attempts, :type => Integer # Only if lock strategy is :failed_attempts
+       # # field :unlock_token,    :type => String # Only if unlock strategy is :email or :both
+       # # field :locked_at,       :type => Time
 
-       # Token authenticatable
-       # field :authentication_token, :type => String
+       # # Token authenticatable
+       # # field :authentication_token, :type => String
 
-       ## Invitable
-       # field :invitation_token, :type => String
+       # ## Invitable
+       # # field :invitation_token, :type => String
       
 
       scope :marketing, where(:marketing => true)
--- init.rb
+++ init.rb
@@ -1,3 +1,7 @@
 Breeze.hook :admin_menu do |menu, user|
   menu << { :name => "Customers", :path => "/admin/account/customers" } if user.can? :manage, Breeze::Content::Item
 end
+
+Breeze.hook :component_info do |component_info|
+	component_info << {:name => 'Breeze Account', :version => Breeze::Account::VERSION }
+end
